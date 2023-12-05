0e51168bb332203f15b43816e997155fada7cff8
R A Omeiza <37030673+raomeiza@users.noreply.github.com>
2023-12-05 05:26:23 +0000

    added socket support for elab

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,51 @@
 import {createServer} from 'http';
 import app from './api'; // index.ts
 import { PORT, BASE_URL } from './config';
+import cors from 'cors'
+import {Server} from 'socket.io';
 
 
 // Spin server
 const server = createServer(app);
+const socketIo = new Server(server, {cors: {origin: '*'}})
+
+socketIo.on('connection', (socket) => {
+  console.log('A client connected');
+
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+  })
+  socket.on('message', (message) => {
+    console.log('New message', message);
+    socketIo.emit('message', message);
+  }).on('error', (err) => {
+    console.log('Socket error', err);
+  });
+
+  socket.on('offer', (offer) => {
+    console.log('New offer', offer);
+    console.log('New offer', offer);
+    socket.broadcast.emit('offer', offer);
+  }).on('error', (err) => {
+    console.log('Socket error', err);
+  });
+
+  socket.on('answer', (answer) => {
+    console.log('New answer', answer);
+    console.log('New answer', answer);
+    socket.broadcast.emit('answer', answer);
+  }).on('error', (err) => {
+    console.log('Socket error', err);
+  });
+
+  socket.on('candidate', (candidate) => {
+    console.log('New candidate', candidate);
+    socket.broadcast.emit('candidate', candidate);
+  }).on('error', (err) => {
+    console.log('Socket error', err);
+  });
+});
+
 server.listen(PORT, () => console.info(`Server listening on ${BASE_URL}`));
 
 // Handle uncaught exceptions
